4c6ba285029739beac5452a0e1a3ff1579a655ac
quarterstar <quarterstar@proton.me>
2025-03-09 10:20:38 -0400

    chore: remove tor relay

--- modules/system/services/tor.nix
+++ modules/system/services/tor.nix
@@ -1,19 +1,5 @@
 { profile, ... }:
 
 if profile.services.tor.enable then {
-  services.tor = {
-    enable = true;
-    openFirewall = true;
-    relay = {
-      enable = true;
-      role = "relay";
-    };
-    settings = {
-      ContactInfo = "";
-      Nickname = "toradmin";
-      ORPort = 9001;
-      ControlPort = 9051;
-      BandWidthRate = "1 MBytes";
-    };
-  };
+  
 } else {}
