c06658757b756a76b4cc256c6705767d160d275b
quarterstar <quarterstar@proton.me>
2025-03-12 00:00:00 +0000

    config(waybar): add config-bottom

--- modules/userspace/config/hyprland/quarterstar/waybar.nix
+++ modules/userspace/config/hyprland/quarterstar/waybar.nix
@@ -1,17 +1,64 @@
-{ pkgs, ... }:
+{ config, profile, pkgs, ... }:
 
-{
+let
+  # Function to generate Waybar custom modules
+  mkAppModule = app: {
+    name = "custom/${app.program}";
+    value = {
+      #format = "<img src='~/.config/waybar/icons/${app.icon}' width='24' height='24'/>";
+      format = "    ";
+      on-click = "${app.program}";
+      # Optional: Add tooltip
+      tooltip = false;
+      # Optional: Add CSS class for styling
+      class = "app-icon";
+    };
+  };
+
+  # Generate all custom modules
+  customModules = builtins.listToAttrs (map mkAppModule profile.env.wm.apps);
+
+  # Generate the modules-center array
+  appModules = map (app: "custom/${app.program}") profile.env.wm.apps;
+in {
   home.file = {
-    ".config/waybar/config" = {
-      source = ../../../../../config/profile/hyprland/quarterstar/waybar/config;
+    ".config/waybar/config-top" = {
+      source = ../../../../../config/profile/hyprland/quarterstar/waybar/config-top;
       executable = false;
       force = true;
     };
 
-    ".config/waybar/style.css" = {
-      source = ../../../../../config/profile/hyprland/quarterstar/waybar/style.css;
+    ".config/waybar/style-top.css" = {
+      source = ../../../../../config/profile/hyprland/quarterstar/waybar/style-top.css;
+      executable = false;
+      force = true;
+    };
+
+    ".config/waybar/style-bottom.css" = {
+      source = ../../../../../config/profile/hyprland/quarterstar/waybar/style-bottom.css;
+      executable = false;
+      force = true;
+    };
+
+    ".config/waybar/icons" = {
+      source = ../../../../../assets/icons;
       executable = false;
       force = true;
     };
   };
+
+  programs.waybar.settings = {
+    "config-bottom" = {
+      reload_style_on_change = true;
+      layer = "bottom";
+      position = "bottom";
+      disable-scroll = true;
+      height = 30;
+      modules-center = appModules;
+      
+      custom = customModules;
+    };
+  };
+
+  xdg.configFile."waybar/config-bottom".text = builtins.toJSON config.programs.waybar.settings."config-bottom";
 }
