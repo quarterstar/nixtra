af24a5f1f4efc9531a11cca4f75dc8b89ac0c7f3
quarterstar <quarterstar@proton.me>
2025-03-16 16:54:33 -0400

    feat(scripts): register new files

--- scripts/reload-waybar.sh
+++ scripts/reload-waybar.sh
@@ -1,9 +1,10 @@
-CONFIG_FILES="$HOME/.config/waybar/config $HOME/.config/waybar/style.css"
+CONFIG_FILES="$HOME/.config/waybar/config-top $HOME/.config/waybar/style-top.css $HOME/.config/waybar/config-bottom $HOME/.config/waybar/style-bottom.css"
 
 trap "pkill waybar" EXIT
 
 while true; do
-    waybar &
-    inotifywait -e create,modify $CONFIG_FILES
-    pkill waybar
+  waybar -c ~/.config/waybar/config-top -s ~/.config/waybar/style-top.css &
+  waybar -c ~/.config/waybar/config-bottom -s ~/.config/waybar/style-bottom.css &
+  inotifywait -e create,modify $CONFIG_FILES
+  pkill waybar
 done
