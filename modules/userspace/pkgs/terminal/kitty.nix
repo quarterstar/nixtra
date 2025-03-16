9cc16cec03bdf21507d35d8d640dfee33fbc721c
quarterstar <quarterstar@proton.me>
2025-03-16 00:00:00 +0000

    fix(kitty): separate config from package

--- modules/userspace/pkgs/terminal/kitty.nix
+++ modules/userspace/pkgs/terminal/kitty.nix
@@ -6,13 +6,4 @@
   ];
 
   programs.kitty.enable = true;
-
-  programs.kitty.settings = {
-    shell = profile.user.shell;
-    background_opacity = 0.25;
-    dynamic_background_opacity = true;
-    confirm_os_window_close = 0;
-  };
-
-  programs.bash.bashrcExtra = "export TERM=\"xterm-256color\"";
 }
