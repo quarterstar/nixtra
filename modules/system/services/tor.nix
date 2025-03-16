d4abdc0fd5662f25a3c34b5a82985b710090d32a
quarterstar <quarterstar@proton.me>
2025-03-16 00:00:00 +0000

    feat(security): implement flavors for tor browser as described in usage

--- modules/system/services/tor.nix
+++ modules/system/services/tor.nix
@@ -1,27 +1,65 @@
-{ profile, ... }:
+{ profile, pkgs, ... }:
 
-if profile.tor.enable then {
-  services.tor = {
-    enable = true;
-    client.enable = true; # Enable Tor as a client
-    torsocks.enable = true;
+if profile.tor.enable then
+{
+  # Define the microsocks service
+  systemd.services.microsocks = {
+    description = "MicroSocks SOCKS5 Proxy";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
 
-    # Change IP address every 10 seconds
-    settings = {
-      CircuitBuildTimeout = 10;
-      LearnCircuitBuildTimeout = 0;
-      MaxCircuitDirtiness = 10;
-      #CookieAuthentication = true;
-      #ControlPort = "auto";
-      #ControlSocket = "/var/run/tor/control";
-      #ControlSocketsGroupWritable = true;
+    serviceConfig = {
+      ExecStart = "${pkgs.proxychains}/bin/proxychains4 ${pkgs.microsocks}/bin/microsocks -p 1080";
+      Restart = "on-failure";
+      RestartSec = "5s";
+      User = "nobody"; # Run as an unprivileged user
     };
-
-    # WARNING: may cause DNS leaks. Only use if absolutely necessary.
-    #torsocks.allowInbound = true;
   };
 
-  systemd.tmpfiles.rules = [
-    "d /var/run/tor 700 root root 10d"
-  ];
+  # Stub service
+  # This is done so that the tor user is created and managed by NixOS
+  services.tor = {
+    enable = true;
+    settings.SOCKSPort = 9000;
+  };
+
+  systemd.services.tor1 = {
+    description = "Tor Service (No Proxy)";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.tor}/bin/tor -f ${pkgs.writeText "torrc1" ''
+        SocksPort 9050
+        DataDirectory /home/tor/tor1
+      ''}";
+      Type = "simple";
+      User = "tor";
+      Group = "tor";
+      Restart = "on-failure";
+      RestartSec = "30s";
+    };
+  };
+
+  systemd.services.tor2 = {
+    description = "Tor Service (Proxy)";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.tor}/bin/tor -f ${pkgs.writeText "torrc2" ''
+        SocksPort 9150
+        DataDirectory /home/tor/tor2
+      ''}";
+      Type = "simple";
+      User = "tor";
+      Group = "tor";
+      Restart = "on-failure";
+      RestartSec = "30s";
+    };
+  };
+
+  # Ensure the data directories exist and have the correct permissions
+  system.activationScripts.torSetup = ''
+    mkdir -p /home/tor
+    chown -R tor:tor /home/tor
+  '';
 } else {}
