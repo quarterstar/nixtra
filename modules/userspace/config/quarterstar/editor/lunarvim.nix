299d801af35e498389178057430e4add90643fcd
quarterstar <quarterstar@proton.me>
2025-03-16 00:00:00 +0000

    config: lunarvim + kitty

---
+++ modules/userspace/config/quarterstar/editor/lunarvim.nix
@@ -0,0 +1,11 @@
+{ profile, pkgs, ... }:
+
+{
+  home.file = {
+    ".config/lvim/config.lua" = {
+      source = ../../../../../config/${profile.user.config}/editor/lunarvim/config.lua;
+      executable = false;
+      force = true;
+    };
+  };
+}
