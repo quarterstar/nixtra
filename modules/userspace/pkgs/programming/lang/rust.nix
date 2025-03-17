6d8aafca4a31ac7e005b5d2ac44cf5b4c4b474fe
quarterstar <quarterstar@proton.me>
2025-03-17 00:00:00 +0000

    feat(rust): overlay for rustup

--- modules/userspace/pkgs/programming/lang/rust.nix
+++ modules/userspace/pkgs/programming/lang/rust.nix
@@ -1,6 +1,19 @@
 { pkgs, ... }:
 
-{
+let
+  rustupOverlay = self: super: {
+    rustup = super.rustup.overrideAttrs (oldAttrs: {
+      postInstall = ''
+        ${oldAttrs.postInstall or ""}
+        $out/bin/rustup component add rust-analyzer
+      '';
+    });
+  };
+in {
+  nixpkgs.overlays = [
+    rustupOverlay
+  ];
+
   home.packages = with pkgs; [
     rustup
     trunk
