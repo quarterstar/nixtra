7cdfbeae7745cc62b96c463b33a78b416c065b46
quarterstar <quarterstar@proton.me>
2025-03-24 00:00:00 +0000

    fix(librewolf): force installation of latest versions of extensions

--- modules/userspace/pkgs/browser/librewolf.nix
+++ modules/userspace/pkgs/browser/librewolf.nix
@@ -10,7 +10,7 @@
 
       Preferences = {
         "browser.sessionstore.max_resumed_crashes" = 0;  # Disable Session Restore feature
-        "browser.sessionhistory.max_entries" = 0; 
+        "browser.sessionhistory.max_entries" = 10; 
         "browser.sessionstore.debug.no_auto_updates" = true; 
         "browser.sessionstore.disable_platform_collection" = true; 
         "cookiebanners.service.mode.privateBrowsing" = 2;  # Block cookie banners in private browsing
@@ -53,42 +53,51 @@
           };
           # Decentraleyes
           "jid1-BoFifL9Vbdl2zQ@jetpack" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/file/4392113/decentraleyes-3.0.0.xpi";
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/decentraleyes/latest.xpi";
             installation_mode = "force_installed";
           };
           # Canvas Blocker
           "CanvasBlocker@kkapsner.de" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/file/4262820/canvasblocker-1.10.1.xpi";
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/canvasblocker/latest.xpi";
             installation_mode = "force_installed";
           };
           # Ghostery
           "firefox@ghostery.com" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/file/4392896/ghostery-10.4.16.xpi";
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/ghostery/latest.xpi";
             installation_mode = "force_installed";
           };
           # ClearURLS
           "{74145f27-f039-47ce-a470-a662b129930a}" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/file/4064884/clearurls-1.26.1.xpi";
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/clearurls/latest.xpi";
             installation_mode = "force_installed";
           };
           # Cookie AutoDelete
           "CookieAutoDelete@kennydo.com" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/file/4040738/cookie_autodelete-3.8.2.xpi";
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/cookie_autodelete/latest.xpi";
             installation_mode = "force_installed";
           };
           # Return Youtube Dislike
           "{762f9885-5a13-4abd-9c77-433dcd38b8fd}" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/file/4371820/return_youtube_dislikes-3.0.0.18.xpi";
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/return_youtube_dislikes/latest.xpi";
             installation_mode = "force_installed";
           };
           # mtab
           "contact@maxhu.dev" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/file/4445472/mtab-1.8.5.xpi";
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/mtab/latest.xpi";
+            installation_mode = "force_installed";
+          };
+          "overbitewx@floodgap.com" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/overbitewx/latest.xpi";
+            installation_mode = "force_installed";
+          };
+          # Startpage
+          "{20fc2e06-e3e4-4b2b-812b-ab431220cada}" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/startpage_private_search/latest.xpi";
             installation_mode = "force_installed";
           };
         } // (if profile.display.themeType == "dark" then {
           "addon@darkreader.org" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/file/4439735/darkreader-4.9.103.xpi";
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
             installation_mode = "force_installed";
           };
       } else {});
