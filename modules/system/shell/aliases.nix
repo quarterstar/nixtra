5f365680c4e645577b07099b7532d3a85cf94edd
quarterstar <quarterstar@proton.me>
2025-03-28 00:00:00 +0000

    fix(security): separate tor aliases and programs

--- modules/system/shell/aliases.nix
+++ modules/system/shell/aliases.nix
@@ -2,10 +2,11 @@
 
 let
   # Function to generate shell aliases
-  torAliases = if profile.tor.aliases.enable then lib.listToAttrs (map (program: {
-    name = program;
-    value = "torsocks ${program}";
-  }) profile.tor.aliases.programs) else {};
+  torAliases = 
+    if profile.tor.aliases.enable then lib.listToAttrs (map (program: {
+      name = program.name;
+      value = "torsocks ${program.name}";
+    }) (builtins.filter (program: !program.hardAlias) profile.tor.aliases.programs)) else {};
 in {
   imports = [
     ../pkgs/cli/security.nix
@@ -16,5 +17,7 @@ in {
   # In addition to shell alias, replace chosen programs
   # with a stub so that GUI applications use the replaced
   # program.
-  environment.systemPackages = map (program: pkgs.writeScriptBin program ''exec torsocks ${program} "$@"'') profile.tor.aliases.programs;
+  environment.systemPackages = map (program:
+    (pkgs.writeScriptBin program.name ''exec torsocks ${program.name} "$@"'')
+  ) (builtins.filter (program: program.hardAlias) profile.tor.aliases.programs);
 }
