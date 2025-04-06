c7b5c6ad788c3000069245f25a612552c7ba9527
quarterstar <quarterstar@proton.me>
2025-04-06 00:00:00 +0000

    fix(nixos): remove bad fonts

--- modules/system/fonts.nix
+++ modules/system/fonts.nix
@@ -1,8 +1,9 @@
 { pkgs, ... }:
 
 {
-  fonts.packages = with pkgs; [
-    font-awesome
-    cm_unicode # Computer Modern font
-  ];
+  fonts = {
+    packages = with pkgs; [
+      font-awesome
+    ];
+  };
 }
